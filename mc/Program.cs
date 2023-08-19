5f065b0183cc1e11f306605225d3686b8caee8bd
marcosalm05 <marcosalmeida12flores@gmail.com>
2023-08-19 13:57:33 -0400

    Inicializacion del proyecto. Instalacion de .NET Core exitosa. Cambios en el archivo .cs con el fin de ejecutar el programa que estare creando

---
+++ mc/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+while (true)
+{
+        Console.Write(">  ");
+    var line = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(line))
+            return;
+        if(line == "1 + 2 * 3" )
+            Console.WriteLine("7");
+        else 
+            Console.WriteLine("Invalid expression!");
+}
+    
